fc5879072f97207000b081ed33cb10e5a37af844
Ata Emir Kaba <ataemirkaba@icloud.com>
2022-07-12 18:41:53 +0300

    efe işlemini gerçekleştirilebileceği saatleri sorgulatan program

--- macos_giris/main.swift
+++ macos_giris/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var islemGunu = 6
+var islemSaati = 19
+
+
+if islemGunu > 0 && islemGunu <= 7
+{
+    if islemGunu <= 5 {
+        if islemSaati >= 9 && islemSaati <= 18 {
+            print("İşleminiz gerçekleştirildi")
+        }
+        else if islemGunu == 5 {
+            print("İşleminiz 3 gün sonra saat 9:00 da gerçekleştirilecektir")
+        }
+        else {
+            print("İşleminiz yarın gün sonra saat 9:00 da gerçekleştirilecektir")
+        }
+    }
+    else if islemGunu == 6 {
+        print("İşleminiz 2 gün sonra saat 9:00 da gerçekleştirilecektir")
+    }
+    else if islemGunu == 7 {
+        print("İşleminiz yarın gün sonra saat 9:00 da gerçekleştirilecektir")
+    }
+}
+else {
+    print("Geçersiz Gün")
+}
